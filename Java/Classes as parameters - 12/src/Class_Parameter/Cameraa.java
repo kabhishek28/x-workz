aef5a15d9748c402983a7c76de2af89b4eef5883
kabhishek28 <kabhishek.eng@gmail.com>
2025-03-19 03:19:49 +0530

    Class as parameter

---
+++ Java/Classes as parameters - 12/src/Class_Parameter/Cameraa.java
@@ -0,0 +1,8 @@
+package Class_Parameter;
+
+public class Cameraa{
+    public void onOfD(Lens lens){
+        System.out.println("class Camera method noOFD : 200D");
+        lens.pixel();
+    }
+}
---
+++ Java/Classes as parameters - 12/src/Class_Parameter/Lens.java
@@ -0,0 +1,7 @@
+package Class_Parameter;
+
+public class Lens {
+    public void pixel(){
+        System.out.println("class Lens method pixel : 1000 pixel");
+    }
+}
