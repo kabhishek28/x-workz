a2c3f96cded7cf118f100d7c5c07430d0052bc2e
kabhishek28 <kabhishek.eng@gmail.com>
2025-03-19 03:21:40 +0530

    Class as parameter

---
+++ Java/Classes as parameters - 12/src/Class_Parameter/Match.java
@@ -0,0 +1,7 @@
+package Class_Parameter;
+
+public class Match {
+    public void homeGround(){
+        System.out.println("class Match method homeGround : Bengaluru");
+    }
+}
---
+++ Java/Classes as parameters - 12/src/Class_Parameter/RCB.java
@@ -0,0 +1,9 @@
+package Class_Parameter;
+
+public class RCB {
+    public void captain(Match match){
+        System.out.println("Class RCB method captain : Rajath");
+        match.homeGround();
+    }
+
+}
