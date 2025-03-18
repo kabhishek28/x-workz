19cb80ac81a95bd89d9233c19b1322e1463601b3
kabhishek28 <kabhishek.eng@gmail.com>
2025-03-19 03:14:57 +0530

    Class as parameter

---
+++ Java/Classes as parameters - 12/src/Class_Parameter/Hotel.java
@@ -0,0 +1,7 @@
+package Class_Parameter;
+
+public class Hotel {
+    public void name(){
+        System.out.println("class Hotel method name : Sagar Hotel");
+    }
+}
---
+++ Java/Classes as parameters - 12/src/Class_Parameter/Mango.java
@@ -0,0 +1,8 @@
+package Class_Parameter;
+
+public class Mango {
+    public  void shake(Hotel hotel){
+        System.out.println("class Mango method shake : Mango Milk Shake");
+        hotel.name();
+    }
+}
