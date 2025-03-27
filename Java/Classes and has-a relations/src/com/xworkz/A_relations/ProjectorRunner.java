a13e0d3d551321f7bc1af828c1955f91d6e5c9c7
kabhishek28 <kabhishek.eng@gmail.com>
2025-03-28 03:30:14 +0530

    A Relation

---
+++ Java/Classes and has-a relations/src/com/xworkz/A_relations/ProjectorRunner.java
@@ -0,0 +1,36 @@
+package com.xworkz.A_relations;
+
+public class ProjectorRunner {
+    public static void main(String[] args) {
+        Projector projector = new Projector();
+
+
+        System.out.println("-------------------------");
+
+        projector.getLens();
+
+        System.out.println("-------------------------");
+
+        projector.getLamp();
+
+        System.out.println("-------------------------");
+
+        projector.getPort();
+
+        System.out.println("-------------------------");
+
+        projector.getWire();
+
+        System.out.println("-------------------------");
+
+        projector.getBoard();
+
+        System.out.println("-------------------------");
+
+        projector.getButton();
+
+        System.out.println("-------------------------");
+
+        projector.getRemote();
+    }
+}
