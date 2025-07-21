3f5517c56bbb83510201eb179bb7d8c59e8163e1
K Abhishek <kabhishek.eng@gmail.com>
2025-07-21 22:55:57 +0530

    Comaparator And Comparable

---
+++ Java/ComparatorAndComparable/src/com/xworkz/comaparatorandcomparable/dto/StudentDTO.java
@@ -0,0 +1,17 @@
+package com.xworkz.comaparatorandcomparable.dto;
+
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
+public class StudentDTO {
+    private String name;
+    private int marks;
+}
